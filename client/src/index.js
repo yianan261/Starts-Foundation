d85884e522f2e7c0acdec118eb30f294c45791fd
yianan261 <yian.chen0131@gmail.com>
2022-12-29 23:24:14 +0800

    edited app.js and react router dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <NotFound></NotFound>,
+  },
+  {
+    path: "/about",
+    element: <About></About>,
+    errorElement: <NotFound></NotFound>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function NotFound() {
+  return <h1>Page Not Found</h1>;
+}
+
+NotFound.propTypes = {};
+export default NotFound;
