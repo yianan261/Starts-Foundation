2f64e55e1253493d402d2de29cdf9fa76b49c331
yianan261 <yian.chen0131@gmail.com>
2023-01-12 19:31:45 +0800

    changed landing page layout and image

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,64 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="mainContainer" role="main">
+        {/* <div
+          id="carouselExampleControls"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="https://i.postimg.cc/KYy4bCnC/DSC1103-292-min.jpg" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://i.postimg.cc/KYy4bCnC/DSC1103-292-min.jpg" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://i.postimg.cc/KYy4bCnC/DSC1103-292-min.jpg" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://i.postimg.cc/KYy4bCnC/DSC1103-292-min.jpg" className="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div> */}
         <div className="banner">
           <div className="subContainer">
             <h1 className="startsFoundation">STARTS FOUNDATION</h1>
             <p className="tagline">
-              Amongst all the chaos, we thrive with solidarity
+              Helping communities in Nepal
             </p>
 
-            <button className="donateBtnMain">Donate</button>
+            <button className="learnBtnMain">Learn More</button>
           </div>
         </div>
       </div>
--- client/src/components/ManageAdmins/ManageAdmins.js
+++ client/src/components/ManageAdmins/ManageAdmins.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ManageAdmins.css";
 
 function ManageAdmins() {
-  return (
-    <>
+  const [admins, setAdmins] = useState(false);
+
+  //If there are already admins
+  //todo: pull admin info from db
+  const renderAdmins = () => {
+    return (
       <main className="manageAdminsDiv">
         <h1 className="manageAdminsTitle">Manage Admins</h1>
         <span className="addAdminSpan">
           <button className="addAdminBtn">add admin</button>
         </span>
       </main>
-    </>
-  );
+    );
+  };
+  return <>{admins? renderAdmins():null}</>;
 }
 
 export default ManageAdmins;
